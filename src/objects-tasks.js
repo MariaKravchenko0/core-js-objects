96c58d5c77ccd1f02991e6ea4725284513c39e2f
Mariia Kravchenko <kravchenkomaria00@gmail.com>
2024-03-14 21:47:34 +0200

    feat: implement fromJSON function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -236,8 +236,15 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const objEntries = JSON.parse(json);
+  const newObj = Object.create(proto);
+
+  Object.entries(objEntries).forEach(([key, value]) => {
+    newObj[key] = value;
+  });
+
+  return newObj;
 }
 
 /**
