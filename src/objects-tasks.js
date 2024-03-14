a5eb5dc5208fe72b6d26ff4b16506c88cdf5b0bb
Mariia Kravchenko <kravchenkomaria00@gmail.com>
2024-03-14 20:59:57 +0200

    feat: implement Rectangle function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -164,8 +164,14 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangle = {
+    width,
+    height,
+    getArea: () => width * height,
+  };
+
+  return rectangle;
 }
 
 /**
